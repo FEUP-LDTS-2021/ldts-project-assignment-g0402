84760cf2bc233d850587e585aaa4e05eb5156d0b
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-01-03 14:58:54 +0000

    Create basic classes and their files. Still without methods and tests.

---
+++ src/main/Object.java
@@ -0,0 +1,10 @@
+public interface Object {
+    private int ID;
+    private int xPosic;
+    private int yPosic;
+    private int height;
+    private int width;
+    private boolean destructible;
+    private int life;
+    Spawn[];
+}
---
+++ src/main/Spawn.java
@@ -0,0 +1,5 @@
+public class Spawn {
+    int xPosic;
+    int yPosic;
+    int ID;
+}
