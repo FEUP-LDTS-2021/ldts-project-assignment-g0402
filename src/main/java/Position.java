f4c104378d5f6207438cd4dc302e3514820afe1d
anacvr <anacristinavramos@outlook.com>
2022-01-10 19:22:06 +0000

    Fixed merge errors

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,84 +1,30 @@
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/**Class Position deals with the x coordinates and y coordinates
- * of any object. It can also set an Object Player to its
- * default position. */
-<<<<<<< HEAD
 
-=======
->>>>>>> 82827a4 (Added Documentation to Position)
-=======
->>>>>>> c0ac190 (Created Test Class for Player AND Created Class Position)
-=======
 /**Class Position deals with the x coordinates and y coordinates
  * of any object. It can also set an Object Player to its
  * default position. */
->>>>>>> 6bdab29 (Added Documentation to Position)
-=======
-/**Class Position deals with the x coordinates and y coordinates
- * of any object. It can also set an Object Player to its
- * default position. */
->>>>>>> d2139a700b0dd6a9b33d7154a4d32a8cba16cbaf
 public class Position {
     private int xPos = 0;
     private int yPos = 0;
     private int xPlayerDefault;
     private int yPlayerDefault;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 82827a4 (Added Documentation to Position)
     /**This constructor defines the Default Coordinates for an
      * Object Player based on the TerminalSize of the game.*/
-=======
->>>>>>> c0ac190 (Created Test Class for Player AND Created Class Position)
-=======
-    /**This constructor defines the Default Coordinates for an
-     * Object Player based on the TerminalSize of the game.*/
->>>>>>> 6bdab29 (Added Documentation to Position)
-=======
-    /**This constructor defines the Default Coordinates for an
-     * Object Player based on the TerminalSize of the game.*/
->>>>>>> d2139a700b0dd6a9b33d7154a4d32a8cba16cbaf
     public Position(TextGraphics game){
         int distanceFromConsoleFloor = 4;
 
         this.xPlayerDefault = (game.getSize().getColumns()/2);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d2139a700b0dd6a9b33d7154a4d32a8cba16cbaf
 
         this.yPlayerDefault = game.getSize().getRows() - distanceFromConsoleFloor;
     }
 
+
+
     /**This constructor defines a Position with  X and Y Coordinates for an
      * unspecified Object based on the values given in the parameters.*/
-<<<<<<< HEAD
-=======
-                //width/2-widthPlayer
-=======
-
->>>>>>> 6bdab29 (Added Documentation to Position)
-        this.yPlayerDefault = game.getSize().getRows() - distanceFromConsoleFloor;
-    }
-
-<<<<<<< HEAD
->>>>>>> c0ac190 (Created Test Class for Player AND Created Class Position)
-=======
-    /**This constructor defines a Position with  X and Y Coordinates for an
-     * unspecified Object based on the values given in the parameters.*/
->>>>>>> 6bdab29 (Added Documentation to Position)
-=======
->>>>>>> d2139a700b0dd6a9b33d7154a4d32a8cba16cbaf
     public Position(int xPos, int yPos){
         this.xPos = xPos;
         this.yPos = yPos;
@@ -100,21 +46,8 @@ public class Position {
         this.yPos = yPos;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     /**This method sets the coordinates in the Object Player given
      * to its Default place taking in consideration the Object Player Size.*/
-=======
->>>>>>> c0ac190 (Created Test Class for Player AND Created Class Position)
-=======
-    /**This method sets the coordinates in the Object Player given
-     * to its Default place taking in consideration the Object Player Size.*/
->>>>>>> 6bdab29 (Added Documentation to Position)
-=======
-    /**This method sets the coordinates in the Object Player given
-     * to its Default place taking in consideration the Object Player Size.*/
->>>>>>> d2139a700b0dd6a9b33d7154a4d32a8cba16cbaf
     public void setPlayerPosDefault(Player player){
         player.setX(this.xPlayerDefault - player.getWidth());
         player.setY(this.yPlayerDefault - player.getHeight());
