3f5d9eaa549fd7bb7f2572bc780d2bf566f0ce0b
anacvr <anacristinavramos@outlook.com>
2022-01-16 19:59:23 +0000

    Added Keyboard Observer Class

---
+++ src/main/java/Game/KeyBoardListener.java
@@ -0,0 +1,5 @@
+package Game;
+
+public interface KeyBoardListener {
+    void keyPressed(Console.Action action);
+}
---
+++ src/main/java/Game/KeyBoardObserver.java
@@ -0,0 +1,28 @@
+package Game;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import static Game.Console.Action.*;
+
+
+public class KeyBoardObserver extends KeyAdapter {
+
+    private KeyBoardListener listener;
+    public KeyBoardObserver() {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        super.keyPressed(e);
+
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_Q, KeyEvent.VK_ESCAPE -> listener.keyPressed(QUIT);
+            case KeyEvent.VK_LEFT -> listener.keyPressed(LEFT);
+            case KeyEvent.VK_RIGHT -> listener.keyPressed(RIGHT);
+            case KeyEvent.VK_UP, KeyEvent.VK_SPACE -> listener.keyPressed(SHOOT);
+        }
+    }
+
+    public void setListener(KeyBoardListener listener) {
+        this.listener = listener;
+    }
+}
