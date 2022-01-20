8acd7e332f58cb7bf457fcef69d61f7e98231fca
anacvr <anacristinavramos@outlook.com>
2022-01-20 16:52:23 +0000

    Added class Instructions and improved abstract class State

--- src/main/java/Game/State.java
+++ src/main/java/Game/State.java
@@ -1,5 +1,6 @@
 package Game;
 
+import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -12,6 +13,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 abstract public class State {
@@ -19,6 +22,10 @@ abstract public class State {
     protected final int width = 32;
     protected final int height = 18;
     private final int sizeFont = 40;
+
+    Collection<SGR> title = new ArrayList<>();
+    Collection<SGR> text = new ArrayList<>();
+    Collection<SGR> selected = new ArrayList<>();
 
     protected void createTerminal(){
         try {
@@ -44,6 +51,11 @@ abstract public class State {
             this.screen.startScreen();
             this.screen.doResizeIfNecessary();
 
+            title.add(SGR.CIRCLED);
+            title.add(SGR.BOLD);
+            text.add(SGR.BORDERED);
+            selected.add(SGR.BLINK);
+
         } catch (IOException | FontFormatException | URISyntaxException e) {
             e.printStackTrace();
         }
@@ -61,4 +73,17 @@ abstract public class State {
     public void close() throws IOException {
         screen.close();
     }
+
+    protected void draw(){
+        try {
+            clear();
+            drawText();
+            refresh();
+            TimeUnit.MILLISECONDS.sleep(20);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void drawText(){}
 }
